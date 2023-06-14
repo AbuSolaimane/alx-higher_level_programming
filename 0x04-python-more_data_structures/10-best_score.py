9488fed4db72059f6b26ed279d7aa653137246bf
AbuSolaimane <fadilimustafa530@gmail.com>
2023-06-14 12:39:15 -0700

    hjdjd

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if len(list(a_dictionary)) == 0:
         return (None)
-    max_key = a_dictionary.keys()[0]
+    max_key = list(a_dictionary.keys())[0]
     max_value = a_dictionary[max_key]
     for k, v in a_dictionary.items():
         if v > max_value:
