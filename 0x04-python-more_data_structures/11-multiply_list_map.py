95ebde374f2b814f325ab1f41fe54a7a6162a399
AbuSolaimane <fadilimustafa530@gmail.com>
2023-06-14 13:02:14 -0700

    hjdjd

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    my_a_dictionary = a_dictionary
-    if key in a_dictionary.keys():
-        del my_a_dictionary[key]
-    return (my_a_dictionary)
+def multiply_list_map(my_list=[], number=0):
+    return map(lambda x : x * number, my_list)
