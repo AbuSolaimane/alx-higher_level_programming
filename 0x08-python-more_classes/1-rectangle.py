9e5b9969dd32012b5ffd85f856ea76f39b36d220
AbuSolaimane <fadilimustafa530@gmail.com>
2023-07-03 13:47:42 -0700

    hjdjd

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """dmnf, fjnfjkef"""
     
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
