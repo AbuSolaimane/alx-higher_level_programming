91af659d3abb38d89bf608e2bd72bfa9358aba17
AbuSolaimane <fadilimustafa530@gmail.com>
2023-08-15 13:34:26 -0700

    hjdjd

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table
-SELECT score, name FROM WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
