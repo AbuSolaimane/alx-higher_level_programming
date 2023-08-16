27f637a601d06d23271127888a1ef49722562434
AbuSolaimane <fadilimustafa530@gmail.com>
2023-08-16 14:24:50 -0700

    hjdjd

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -3,6 +3,6 @@ SELECT name FROM tv_genres
 	WHERE name NOT IN (SELECT name FROM tv_genres tg
 		LEFT JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
 		LEFT JOIN tv_shows ts ON tsg.show_id = ts.id
-		WHERE tv_shows.title = 'Dexter')
+		WHERE ts.title = 'Dexter')
 	GROUP BY name
 	ORDER BY name ASC;
