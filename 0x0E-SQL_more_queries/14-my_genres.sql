990413e2c5a8d1a994392f0c64be7ae03b89bb81
AbuSolaimane <fadilimustafa530@gmail.com>
2023-08-16 14:11:29 -0700

    hjdjd

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- lists all genres 
 SELECT tv.name FROM tv_genres tv
-	INNER JOIN tv_show_genres tvg
+	INNER JOIN tv_show_genres AS tvg
 	ON tv.id = tvg.genre_id
-	INNER JOIN tv_shows tv2
+	INNER JOIN tv_shows AS tv2
 	ON tvg.show_id = tv2.id
-	WHERE tv.title = "Dexter"
+	WHERE tv.title = 'Dexter'
 	ORDER BY tv.name;
