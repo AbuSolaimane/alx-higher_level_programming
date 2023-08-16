a2518438b4fdc3d6e05b1e353611b6513c5c4a22
AbuSolaimane <fadilimustafa530@gmail.com>
2023-08-16 14:07:20 -0700

    hjdjd

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Lists all genres
 SELECT tv.name AS `genre`,
 	COUNT(*) AS `number_of_shows`
-	FROM `tv_genres` tv INNER JOIN `tv_show_genres` tvg
+	FROM `tv_genres` AS tv INNER JOIN `tv_show_genres` AS tvg
 	ON tv.id = tvg.genre_id
 	GROUP BY tv.name
 	ORDER BY `number_of_show` DESC;
