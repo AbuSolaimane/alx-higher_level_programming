c616f490d2978a3bdced86a4a49243f7f60d2b8a
AbuSolaimane <fadilimustafa530@gmail.com>
2023-08-16 12:37:00 -0700

    hjdjd

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,6 @@
 -- Creates user
-
 CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY 'user_0d_1_pwd';
+-- give
 GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
+-- flush
 FLUSH PRIVILEGES;
