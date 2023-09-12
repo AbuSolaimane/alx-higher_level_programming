6120abeb774e497dd2e40f80a83089acaae1c10d
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-12 06:06:36 -0700

    hjdjd

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const sortedArgs = process.argv
+    .slice(2, process.argv.length)
+    .map(parseInt)
+    .sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
+}
