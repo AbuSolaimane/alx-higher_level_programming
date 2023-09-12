711b77d9fa7ed3fddda50345e54b18cc73afb36a
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-12 05:55:53 -0700

    hjdjd

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+function factorial (n) {
+  return isNaN(n) || n === 0 ? 1 : n * factorial(n - 1);
+}i
+
+console.log(factorial(parseInt(process.argv[2])));
