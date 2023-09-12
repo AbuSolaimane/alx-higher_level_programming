ab8c2916799ad2204ebfd056e716477c8a168f91
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-12 05:34:45 -0700

    hjdjd

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,3 +1,10 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+const x = process.argv[2];
+
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(x); i++) {
+    console.log('C is fun');
+  }
+}
