2cad42c72ef34c48d2cf3de6464ddd4de5da46a7
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-16 11:55:07 -0700

    hjdjd

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     the main
     """
     db = driver.connect(
-        host="localhost", user=argv[1], password=argv[2],
+        host="localhost", user=argv[1], passwd=argv[2],
         database=argv[3], port=3306)
     # cursor object created to interact with the database
     cursor = db.cursor()
