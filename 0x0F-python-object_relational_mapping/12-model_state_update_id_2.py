a847087eaa7dd3dc7de0e0cc42bb726621966c89
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-16 14:38:43 -0700

    hjdjd

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+this is module
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    the main
+    """
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+    # Create the database engine using the url
+    engine = create_engine(url)
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_ = session.query(State).filter(State.id == 2).first()
+    state_.name = "New Mexico"
+    session_.commit()
