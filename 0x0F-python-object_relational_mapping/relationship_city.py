92846e5555e1def283c5c520a2b218de2da078b3
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-16 15:39:21 -0700

    hjdjd

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,7 +4,7 @@ this is a module
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from relationship_state import Base, State
 
 
 class City(Base):
