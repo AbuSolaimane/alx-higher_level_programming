f0a38090006a95a76fbbcdadfb155da7c5639b93
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-16 12:56:22 -0700

    hjdjd

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+this is a module
+"""
+
+import MySQLdb as driver
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    the main
+    """
+    db_ = driver.connect(
+        host="localhost", user=argv[1], passwd=argv[2],
+        db=argv[3], port=3306)
+    # cursor object created to interact with the database
+    cursor = db_.cursor()
+    # Execute a simple SELECT query
+    query = "SELECT c.id, c.name, s.name FROM \
+            cities c INNER JOIN states s ON \
+            c.state_id = s.id WHERE s.name \
+            LIKE BINARY %(name)s  ORDER BY c.id ASC"
+    cursor.execute(query, {'name': argv[4]})
+    # Fetch and print the results
+    states = cursor.fetchall()
+    if states is not None:
+        for row in states:
+            print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db_.close()
