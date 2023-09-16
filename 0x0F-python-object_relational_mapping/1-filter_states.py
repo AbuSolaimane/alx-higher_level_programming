bf6c107340a80ea55c1d58eeb870fd716ba92176
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-16 12:04:28 -0700

    hjdjd

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this is a module
+"""
+
+import MySQLdb as driver
+from sys import argv
+
+if __name__ == '__main__':
+    """
+    the main
+    """
+    db_ = driver.connect(
+        host="localhost", user=argv[1], passwd=argv[2],
+        db=argv[3], port=3306)
+    # cursor object created to interact with the database
+    cursor = db_.cursor()
+    # Execute a simple SELECT query
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+    # Fetch and print the results
+    states = cursor.fetchall()
+    for row in states:
+        print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db_.close()
