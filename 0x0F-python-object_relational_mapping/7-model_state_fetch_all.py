aa1f5f11d6db6d3b2b3169ed7493f8cbcb53e64b
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-16 13:57:32 -0700

    hjdjd

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+this is module
+"""
+
+from sys import argv
+from model_state import State, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    the main
+    """
+    url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+    # Create the database engine using the url
+    engine = create_engine(url, echo=True)
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(state.id, state.name))
