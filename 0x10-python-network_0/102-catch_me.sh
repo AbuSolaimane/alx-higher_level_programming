4d62ed360fcbe84bf9ad565228903e1eac3e0d7a
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-28 14:57:58 -0700

    hjdjd

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# use curl to send a request and displays the size of the response
-curl -s "$1" | wc -c
+# use curl to send a request
+curl -o /dev/null -w "You got me!" -s 0.0.0.0:5000/catch_me
