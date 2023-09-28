ac62d1e931d67ad77239cd8eac0356da16af15f5
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-28 12:27:18 -0700

    hjdjd

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# use curl to send a request and displays the size of the response
-curl -s "$1" | wc -c
+# use curl to send a request and display all allowed methods
+curl -sI "$1" | grep -i "Allow:" | awk '{print $2}'
