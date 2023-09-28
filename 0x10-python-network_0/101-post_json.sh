a7bae65355ba5481a3745de99e4e8fb73e516040
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-28 14:23:40 -0700

    hjdjd

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # use curl to send a request and displays the size of the response
-curl -s -X POST -H "Content-Type: application/json" -d "%(cat $2)" "$1"
+curl -s -X POST -H "Content-Type: application/json" -d "%(cat "$2")" "$1"
