88488d7e753109ffb2e9d441d16bf55f59142cab
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 15:04:22 -0700

    hjdjd

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -2,3 +2,20 @@
 """
     a python module
 """
+from sys import argv
+from requests import post
+
+
+if __name__ == "__main__":
+    letter = "" if len(argv) == 1 else argv[1]
+    payload = {"q": letter}
+
+    res = post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = res.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
