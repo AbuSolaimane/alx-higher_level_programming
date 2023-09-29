7e67d6c1b00c6a41026d43e73770f3c062faa571
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 14:43:24 -0700

    hjdjd

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,3 +2,16 @@
 """
     a python module
 """
+from sys import argv
+from urllib.error import HTTPError
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    req = Request(url)
+    try:
+        with urlopen(req) as res:
+            print(res.read().decode("ascii"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
