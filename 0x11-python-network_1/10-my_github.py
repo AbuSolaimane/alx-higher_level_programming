230ef9ee9e9a5d14a683da4806d2a820e2fdfbf8
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 15:07:19 -0700

    hjdjd

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,3 +2,12 @@
 """
     a python module
 """
+from sys import argv
+from requests import get
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    res = requests.get("https://api.github.com/user", auth=auth)
+    print(res.json().get("id"))
