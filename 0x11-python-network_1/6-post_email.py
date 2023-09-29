07233e4e379ea6a769aa9bfad30b2f92e5757d03
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 14:57:05 -0700

    hjdjd

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,3 +2,12 @@
 """
     a python module
 """
+from sys import argv
+from requests import post
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    res = post(url, data=value)
+    print(res.text)
