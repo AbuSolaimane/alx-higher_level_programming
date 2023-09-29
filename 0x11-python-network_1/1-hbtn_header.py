15c328be1344853d87eced19018c4f91c480342f
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 14:20:39 -0700

    hjdjd

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,3 +2,13 @@
 """
     a python module
 """
+
+import sys
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = Request(url)
+    with urlopen(req) as res:
+        print(dict(res.headers).get("X-Request-Id"))
