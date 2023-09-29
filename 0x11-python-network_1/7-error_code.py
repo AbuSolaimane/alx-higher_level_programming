46acc0b68fb4f54fdc325a6b5067bbc09f584913
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 15:00:00 -0700

    hjdjd

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -2,3 +2,14 @@
 """
     a python module
 """
+from sys import argv
+from requests import get
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    res = get(url)
+    if res.status_code >= 400:
+        print("Error code: {}".format(res.status_code))
+    else:
+        print(res.text)
