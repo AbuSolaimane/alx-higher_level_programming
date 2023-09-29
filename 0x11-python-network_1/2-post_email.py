7564eec80a6f0d7937ca2d886c70d22c27e803d0
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 14:33:50 -0700

    hjdjd

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,3 +2,15 @@
 """
     a python module
 """
+from sys import argv
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    value = {"email": argv[2]}
+    data = urlencode(value).encode("ascii")
+    req = Request(url, data)
+    with urlopen(req) as res:
+        print(res.read().decode("utf-8"))
