c4ed60f951b77248dfa8a2bf918cd93c93a0c67d
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 14:45:25 -0700

    hjdjd

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,3 +2,10 @@
 """
     a python module
 """
+from requests import get
+
+if __name__ == "__main__":
+    r = get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
