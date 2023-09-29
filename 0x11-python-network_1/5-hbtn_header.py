4d051c2d0ec161a6414464cbccb79eb1539614b7
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 14:48:12 -0700

    hjdjd

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,3 +2,12 @@
 """
     a python module
 """
+from sys import argv
+from requests import get
+
+
+if __name__ == "__main__":
+    
+    url = argv[1]
+    res = get(url)
+    print(res.headers.get("X-Request-Id"))
