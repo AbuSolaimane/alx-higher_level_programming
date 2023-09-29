c5da71e7f3210f9da32b888e37f0d4593358091b
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-29 15:19:10 -0700

    hjdjd

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -2,3 +2,17 @@
 """
     a python module
 """
+from sys import argv
+from requests import get
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        argv[2], argv[1])
+
+    res = get(url)
+    commits = res.json()
+    for i in range(10):
+        print("{}: {}".format(
+            commits[i].get("sha"),
+            commits[i].get("commit").get("author").get("name")))
