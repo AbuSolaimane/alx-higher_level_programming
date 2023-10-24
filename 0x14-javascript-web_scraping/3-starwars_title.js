7f6e2cdfce6e95aadfc76f465434df1184d183b9
AbuSolaimane <fadilimustafa530@gmail.com>
2023-10-24 12:57:12 -0700

    hjdjd

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+
 const req = require('request');
 const id = process.argv[2];
-const myUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`
+const myUrl = `https://swapi-api.alx-tools.com/api/films/${id}`
 
 req.get(myUrl, (err, res, body) => {
   if (err) {
